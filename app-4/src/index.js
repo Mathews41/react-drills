9278ae1e767e4a82cef6781fdf9e60c3c8ad8570
Mark Mathews <marklmathews4@gmail.com>
2019-09-08 16:31:23 -0600

    finished react drills

--- app-4/src/index.js
+++ app-4/src/index.js
@@ -1,8 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React, {Component} from 'react';
+class Login extends Component {
+    constructor() {
+        super();
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+        this.state ={
+            username:"",
+            password:""
+        };
+        this.handleLogin = this.handleLogin.bind(this)
+        
+    }
+    handleUsernameChange(name) {
+        this.setState({username: name });
+    }
+    handlePasswordChange(pass) {
+    this.setState({password: pass});
+    }
+    handleLogin(){
+        alert(`Username: ${this.state.username} Password: ${this.state.password}`)
+    }
+
+    render(){
+        return (
+            <div>
+                <input onChange = {e => this.handleUsernameChange(e.target.value)}type='text' />
+
+                <input
+                onChange={e => this.handleUsernameChange(e.target.value)}type="text" />
+
+                <button onClick={this.handleLogin}>Login</button>
+            </div>
+        )
+    }
+}
+export default Login;
